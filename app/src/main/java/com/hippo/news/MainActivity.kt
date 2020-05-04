78b7c7a4507ef5ec73412ddf9a77df49789f8c93
matttrue <mrdeevs@gmail.com>
2020-05-04 00:11:32 -0500

    adding basic android project files

---
+++ app/src/main/java/com/hippo/news/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hippo.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.news_main)
+    }
+}
