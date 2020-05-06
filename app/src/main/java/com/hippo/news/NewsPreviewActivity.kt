c717d1bf945bd9c03115278420b4b1bb28af4bf4
matttrue <mrdeevs@gmail.com>
2020-05-05 22:10:03 -0500

    news preview: working correctly with android web view. pretty radgit diff! this is the first MVP of a working news aggregator and reader...excited about where to go from here

--- app/src/main/java/com/hippo/news/NewsPreviewActivity.kt
+++ app/src/main/java/com/hippo/news/NewsPreviewActivity.kt
@@ -3,6 +3,7 @@ package com.hippo.news
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
 class NewsPreviewActivity : AppCompatActivity() {
@@ -17,7 +18,8 @@ class NewsPreviewActivity : AppCompatActivity() {
 
         if(intent != null && intent.getStringExtra(KEY_URL) != null) {
             val storyUrl = intent.getStringExtra(KEY_URL)
-            Log.e("test", "preview found url: $storyUrl")
+            val myWebView: WebView = findViewById(R.id.preview_webview)
+            myWebView.loadUrl(storyUrl)
         }
     }
 }
